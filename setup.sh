d2101942d5055af1fd80eb532deb7fc413d8e3b0
Jirka Koutný <jirkakoutny@gmail.com>
2021-09-05 08:22:32 +0200

    python in venv path fixed

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 PROJECT_DIR="/home/osmc/kodi_backup"
-PYTHON_EXECUTABLE="$PROJECT_DIR/bin/python3"
+PYTHON_EXECUTABLE="$PROJECT_DIR/.venv/bin/python3"
 
 echo "Setting up crontab"
 (crontab -l 2>/dev/null; echo "0 5 * * * AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} "$PYTHON_EXECUTABLE" "$PROJECT_DIR/backup.py" >/dev/null 2>&1") | crontab -
